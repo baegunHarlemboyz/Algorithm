171c052757f0478eafb20100bf909784a4a74394
T5643 <145423261+baegunHarlemboyz@users.noreply.github.com>
2024-04-24 17:21:28 +0900

    [Bronze V] Title: N 찍기, Time: 1040 ms, Memory: 33052 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        
+        int a = in.nextInt();
+
+        for(int i=1; i<=a; i++){
+            System.out.println(i);
+        }
+
+
+    }
+}
