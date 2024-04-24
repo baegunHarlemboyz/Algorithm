5d10485cf5e65fd52da1a560904fe0fd565b1225
T5643 <145423261+baegunHarlemboyz@users.noreply.github.com>
2024-04-24 17:11:46 +0900

    [Bronze V] Title: 구구단, Time: 240 ms, Memory: 18500 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        
+        in.close();
+
+        for(int i=1; i<=9; i++){
+            System.out.println(n + " * " + i + " = " + n*i);
+           
+        }
+        
+    
+       
+    }
+}
